2950b66f59e4eae3fa2b0121e5b671b39e8444ac
unicorn-kris <kristinafcb1@gmail.com>
2022-02-26 00:40:14 +0400

    add test projects and realize service test classes

---
+++ ServiceTests/ProductServiceTest.cs
@@ -0,0 +1,107 @@
+using CustomerAleksandr.TestgRPCApplication.Services;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using Grpc.Core.Utils;
+using Logic.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    public class ProductServiceTest
+    {
+        private Mock<IProductService> _mockProductService;
+        private ILogger<ProductService> _loggerProductService;
+        private ProductService _productService;
+        private ServerCallContext _testServerCallContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockProductService = new Mock<IProductService>();
+            _mockProductService.Setup(m => m.AddProduct(It.IsAny<Logic.Entities.Product>())).Returns(1);
+            _mockProductService.Setup(m => m.GetAll()).Returns(new List<Logic.Entities.Product> { new Logic.Entities.Product { Title = "Title", Count = 10, Price = 12, Id = 1 } });
+            _mockProductService.Setup(m => m.DeleteProduct(1));
+            _mockProductService.Setup(m => m.BuyProduct(It.IsAny<int>(), It.IsAny<int>()));
+            _mockProductService.Setup(m => m.GetProductById(1)).Returns(new Logic.Entities.Product { Title = "Title", Count = 10, Price = 12, Id = 1 });
+
+            _testServerCallContext = TestServerCallContext.Create("fooMethod", null, DateTime.UtcNow.AddHours(1), new Metadata(), CancellationToken.None, "127.0.0.1", null, null, (metadata) => TaskUtils.CompletedTask, () => new WriteOptions(), (writeOptions) => { });
+
+            _productService = new ProductService(_loggerProductService, _mockProductService.Object);
+        }
+
+        #region add
+        [Test]
+        public async Task AddProductTest()
+        {
+            // Act
+            var response = await _productService.AddProduct(
+                new Product { Title = "Title", Count = 10, Price = 12 }, _testServerCallContext);
+
+            // Assert
+            Assert.AreEqual(1, response.Id);
+        }
+
+        #endregion
+
+        #region get all
+        [Test]
+        public async Task GetAllProductTest()
+        {
+            // Act
+            var response = await _productService.GetProducts(new Empty(), _testServerCallContext);
+
+            // Assert
+            Assert.AreEqual(1, response.ProductsList.Count);
+            Assert.AreEqual(1, response.ProductsList[0].Id);
+        }
+
+        #endregion
+
+        #region Delete
+        [Test]
+        public async Task DeleteProductTest()
+        {
+            // Act
+            var response = await _productService.DeleteProduct(new ProductId { Id = 1 }, _testServerCallContext);
+
+            // Assert
+            _mockProductService.Verify(p => p.DeleteProduct(1));
+        }
+
+        #endregion
+
+        #region Buy
+        [Test]
+        public async Task BuyProductTest()
+        {
+            // Act
+            var response = await _productService.BuyProduct(new BuyProductRequest { UserId = 1, ProductId = 1 }, _testServerCallContext);
+
+            // Assert
+            _mockProductService.Verify(p => p.BuyProduct(1, 1));
+        }
+
+        #endregion
+
+        #region Get by id
+        [Test]
+        public async Task GetByIdProductTest()
+        {
+            // Act
+            var response = await _productService.GetProductById(new ProductId { Id = 1 }, _testServerCallContext);
+
+            // Assert
+            _mockProductService.Verify(p => p.GetProductById(1));
+            Assert.AreEqual(1, response.Id);
+        }
+
+        #endregion
+    }
+}
---
+++ ServiceTests/UserServiceTest.cs
@@ -0,0 +1,79 @@
+﻿using CustomerAleksandr.TestgRPCApplication.Services;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using Grpc.Core.Utils;
+using Logic.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    class UserServiceTest
+    {
+        Mock<IUserService> _mockUserService;
+        ILogger<UserService> _loggerUserService;
+        UserService _userService;
+        ServerCallContext _testServerCallContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _mockUserService.Setup(m => m.AddUser(It.IsAny<Logic.Entities.User>())).Returns(1);
+            _mockUserService.Setup(m => m.GetAll()).Returns(new List<Logic.Entities.User> { new Logic.Entities.User { Name = "Name", Surname = "Surname", Id = 1 } });
+            _mockUserService.Setup(m => m.GetUserById(1)).Returns(new Logic.Entities.User { Name = "Name", Surname = "Surname", Id = 1 });
+
+            _testServerCallContext = TestServerCallContext.Create("fooMethod", null, DateTime.UtcNow.AddHours(1), new Metadata(), CancellationToken.None, "127.0.0.1", null, null, (metadata) => TaskUtils.CompletedTask, () => new WriteOptions(), (writeOptions) => { });
+
+            _userService = new UserService(_loggerUserService, _mockUserService.Object);
+        }
+
+        #region add
+        [Test]
+        public async Task AddUserTest()
+        {
+            // Act
+            var response = await _userService.AddUser(
+                new User { Name = "Name", Surname = "Surname" }, _testServerCallContext);
+
+            // Assert
+            Assert.AreEqual(1, response.Id);
+        }
+
+        #endregion
+
+        #region get all
+        [Test]
+        public async Task GetAllUserTest()
+        {
+            // Act
+            var response = await _userService.GetUsers(new Empty(), _testServerCallContext);
+
+            // Assert
+            Assert.AreEqual(1, response.UsersList.Count);
+            Assert.AreEqual(1, response.UsersList[0].Id);
+        }
+
+        #endregion
+
+        #region Get by id
+        [Test]
+        public async Task GetByIdUserTest()
+        {
+            // Act
+            var response = await _userService.GetUserById(new UserId { Id = 1 }, _testServerCallContext);
+
+            // Assert
+            _mockUserService.Verify(p => p.GetUserById(1));
+            Assert.AreEqual(1, response.Id);
+        }
+
+        #endregion
+    }
+}
